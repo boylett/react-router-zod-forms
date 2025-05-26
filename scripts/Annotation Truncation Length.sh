84979d947b917e3ab9c1c161c8ba56a34f66051f
boylett <ryan@boylett.dev>
2025-05-26 20:43:50 +0100

    remove debug patches

--- scripts/Annotation Truncation Length.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-FROM="var defaultMaximumTruncationLength = 160;"
-TO="var defaultMaximumTruncationLength = 1e6;"
-
-sed -i -e "s/$FROM/$TO/g" "node_modules/typescript/lib/typescript.js"
-
-echo "Annotation Truncation Length patched successfully"
